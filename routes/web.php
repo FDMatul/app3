ff7a8de6d6d0159e39ec9b5f8333557192a335bf
Morales <fmoralesmatul@sensata.com>
2022-06-07 16:59:46 -0700

    Se modifico rutas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+return view('welcome');
+});
+
+Route::get('/custom',function (){
+
+    return ("custom");
+
 });
